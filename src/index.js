ebf39bd35bee2bb8a6e0a4ce76dad192c3aad85e
klvklv <une_dina@mail.ru>
2022-01-26 22:56:10 +0200

    feat: implement morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	let str = "";
+   let arr = expr.split("");
+	let arrLetterCode = arr.splice(0, 10);
+	while(arrLetterCode.length == 10) {
+		str = str + decoderLetter(arrLetterCode);
+		arrLetterCode = arr.splice(0, 10);
+	}
+	return str;
 }
+
+function decoderLetter (arrLetterCode) {
+	if(arrLetterCode[0] == "*") return " ";
+	while(arrLetterCode[0] == 0) {
+		arrLetterCode.splice(0, 1);
+	}
+	let dotDashLetter = "";
+	for(let i = 1; i < arrLetterCode.length; i = i +2) {
+		if(arrLetterCode[i] == 0) dotDashLetter += ".";
+		else dotDashLetter += "-";
+	}
+	return MORSE_TABLE[dotDashLetter];
+}
+
+
+
 
 module.exports = {
     decode
